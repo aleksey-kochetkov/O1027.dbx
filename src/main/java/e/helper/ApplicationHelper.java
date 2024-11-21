51c3d4676eae4cbe2b3fe6fc2141f5313106e095
akochetkov <akochetkov@ceki.ru>
2024-11-21 21:21:03 +0300

    2024-11-21

--- src/main/java/e/helper/ApplicationHelper.java
+++ src/main/java/e/helper/ApplicationHelper.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 
 public class ApplicationHelper {
+  private static final String FILE_ELEMENTS = "elements.xml";
   private static List<e.model.Element> O1027Elements = new ArrayList<>();
 
   static {
@@ -23,13 +24,27 @@ public class ApplicationHelper {
     }
   }
 
+  public static <T> Enumeration<T> enumeration(T... args) {
+    return Collections.enumeration(Arrays.asList(args));
+  }
+
+  public static InputStream newSequenceInputStream(
+                  String openingTag, InputStream in, String closingTag) {
+    return new SequenceInputStream(enumeration(
+            (InputStream)new ByteArrayInputStream(openingTag.getBytes()),
+                   in, new ByteArrayInputStream(closingTag.getBytes())));
+  }
+
   private static void initFileProperties() {
   }
 
   private static void initFileElements()
-                        throws IOException, ParserConfigurationException {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+         throws IOException, ParserConfigurationException, SAXException {
+    DocumentBuilderFactory
+        factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = factory.newDocumentBuilder();
+    Document document = builder.parse(newSequenceInputStream("<file>",
+                         new FileInputStream(FILE_ELEMENTS), "</file>"));
   }
 
   public static List<e.model.Element> getPropertyO1027Elements() {
