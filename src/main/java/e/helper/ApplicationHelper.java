9bffa1027abf7d9f331bac3844350fe2844bef03
akochetkov <akochetkov@ceki.ru>
2024-11-22 15:47:16 +0300

    2024-11-22

--- src/main/java/e/helper/ApplicationHelper.java
+++ src/main/java/e/helper/ApplicationHelper.java
@@ -1,11 +1,21 @@
 package e.helper;
 
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.SequenceInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class ApplicationHelper {
   private static final String FILE_ELEMENTS = "elements.xml";
@@ -19,7 +29,8 @@ public class ApplicationHelper {
     initFileProperties();
     try {
       initFileElements();
-    } catch (IOException | ParserConfigurationException exception) {
+    } catch (IOException | ParserConfigurationException
+                                              | SAXException exception) {
       throw new RuntimeException(exception);
     }
   }
@@ -45,6 +56,40 @@ public class ApplicationHelper {
     DocumentBuilder builder = factory.newDocumentBuilder();
     Document document = builder.parse(newSequenceInputStream("<file>",
                          new FileInputStream(FILE_ELEMENTS), "</file>"));
+    NodeList elements = document.getElementsByTagName("element");
+    for (int i = 0; i < elements.getLength(); i++) {
+      NodeList children = elements.item(i).getChildNodes();
+      if (children.getLength() != 2) {
+        throw new IllegalStateException(String.format(
+             "<element> children:%d; expected 2", children.getLength()));
+      }
+      String remotePath = null, localPath = null;
+      for (int i_property = 0; i_property < 2; i_property++) {
+        Node property = (Element)children.item(i_property);
+        Node attribute = property.getAttributes().item(0);
+        switch (property.getNodeName()) {
+        case "remote":
+          remotePath = getPath(attribute);
+          break;
+        case "local":
+          localPath = getPath(attribute);
+          break;
+        }
+      }
+      String name = remotePath
+          .substring(remotePath.lastIndexOf('/') + 1);
+      e.model.Element element = new e.model.Element(name,
+                remotePath.substring(0, remotePath.lastIndexOf('/') + 1),
+                 localPath.substring(0, localPath.lastIndexOf('/') + 1));
+      O1027Elements.add(element);
+    }
+  }
+
+  private static String getPath(Node node) {
+    if (!"path".equals(node.getNodeName())) {
+      throw new IllegalStateException();
+    }
+    return node.getNodeValue();
   }
 
   public static List<e.model.Element> getPropertyO1027Elements() {
--- src/main/java/e/model/Element.java
+++ src/main/java/e/model/Element.java
@@ -1,4 +1,13 @@
 package e.model;
 
 public class Element {
+  private String name;
+  private String remoteFolder;
+  private String localFolder;
+
+  public Element(String name, String remoteFolder, String localFolder) {
+    this.name = name;
+    this.remoteFolder = remoteFolder;
+    this.localFolder = localFolder;
+  }
 }
